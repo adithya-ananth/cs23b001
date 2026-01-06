6bb21ead6083ba188bb176a5578b87bd39e27751
adithya-ananth <cs23b001@iittp.ac.in>
2026-01-06 14:52:35 +0530

    Created carrier signal

--- cs23b001_nrz_am.py
+++ cs23b001_nrz_am.py
@@ -4,3 +4,13 @@ import matplotlib.pyplot as plt
 data = [1,0,1,0,1,1,0]
 data_new = np.array([-1 if i == 0 else 1 for i in data])
 message = np.repeat(data_new, 200)
+
+# Carrier signal
+fc = 20
+
+N = 7 * 200
+T = 7
+
+t = np.linspace(0, T, N, endpoint = False)
+
+carrier_signal = np.cos(2* np.pi * fc * t)
