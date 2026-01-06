3d9b852d2a1d0e5f1bd475c8e44ba4e8409beb86
adithya-ananth <cs23b001@iittp.ac.in>
2026-01-06 15:16:30 +0530

    Created sub plots and stored in external file as amplots.pdf

--- cs23b001_nrz_am.py
+++ cs23b001_nrz_am.py
@@ -17,3 +17,35 @@ carrier_signal = np.cos(2* np.pi * fc * t)
 
 # Amplitude Modulation
 am_signal = message * carrier_signal
+
+# Plots
+# Create a figure with three subplots
+plt.figure(figsize=(12, 10))
+
+# Subplot 1: Message Signal
+plt.subplot(3, 1, 1)
+plt.plot(np.arange(len(message)), message)
+plt.title('Message Signal')
+plt.xlabel('Sample Index')
+plt.ylabel('Amplitude')
+plt.grid(True)
+
+# Subplot 2: Carrier Signal
+plt.subplot(3, 1, 2)
+plt.plot(t, carrier_signal)
+plt.title(f'Carrier Signal (fc = {fc} Hz)')
+plt.xlabel('Time (s)')
+plt.ylabel('Amplitude')
+plt.grid(True)
+
+# Subplot 3: AM Modulated Signal
+plt.subplot(3, 1, 3)
+plt.plot(t, am_signal)
+plt.title('AM Modulated Signal')
+plt.xlabel('Time (s)')
+plt.ylabel('Amplitude')
+plt.grid(True)
+
+plt.tight_layout()
+plt.savefig('amplots.pdf') # Save the figure as amplots.pdf
+plt.show()
